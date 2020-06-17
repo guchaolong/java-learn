4d0105017b9940bafeec457d95ee431f24c151cf
guchaolong <guclno1@qq.com>
2020-06-17 22:16:55 +0800

    dssfs

--- src/main/java/com/guchaolong/algorithm/dfs_bfs/Node.java
+++ src/main/java/com/guchaolong/algorithm/dfs_bfs/Node.java
@@ -19,10 +19,11 @@ public class Node {
     public int val;
     public List<Node> children;
 
-    public Node() {}
+    public Node() {
+    }
 
-    public Node(int _val,List<Node> _children) {
+    public Node(int _val, List<Node> _children) {
         val = _val;
         children = _children;
-
-}}
+    }
+}
