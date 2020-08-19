f516dfbf326719d2015ad8bb049f20ed208b7079
guchaolong <guclno1@qq.com>
2020-08-19 14:02:11 +0800

    sdf

--- src/main/java/com/guchaolong/javalearn/cast/Casting.java
+++ src/main/java/com/guchaolong/javalearn/cast/Casting.java
@@ -19,17 +19,22 @@ public class Casting {
 
     }
     public static void main(String[] args) {
-        byte b = 1;
-        short s = b;
-        byte b2 = (byte)s;
-        short s2 = b;
+        //把byte类型的赋值给short或char类型
+        byte b1 = 1;
+        short s1 = b1;
+        char c1 = (char)b1;
 
-        char c = (char)b;
+        //short类型转成byte或char，都需要强转
+        short s2 = 1;
+        byte b2 = (byte)s2;
+        char c2 = (char)s2;
 
-        char c2 ='r';
-        byte b3 = (byte)c2;
+        //char类型转成其他，都需要强转
+        char c3 = 'a';
+        byte b3 = (byte)c3;
+        short s3 = (short)c3;
 
-        char c3 = (char)s;
+        //总结，byte-short-int-long-float-double遵循规则，有char都必须强转
 
     }
 }
