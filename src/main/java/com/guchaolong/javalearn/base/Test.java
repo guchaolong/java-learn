45ef21cd6dcaec316f6ebb0abf97d74cfb020866
guchaolong <guclno1@qq.com>
2020-08-19 18:47:35 +0800

    dsf

--- src/main/java/com/guchaolong/javalearn/base/Test.java
+++ src/main/java/com/guchaolong/javalearn/base/Test.java
@@ -1,5 +1,7 @@
 package com.guchaolong.javalearn.base;
 
+import java.lang.reflect.Constructor;
+
 /**
  * Description:
  *
@@ -7,7 +9,7 @@ package com.guchaolong.javalearn.base;
  * @date 2020/8/19 14:22
  */
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //数组定义
         int[] arr1 = new int[3];
 
@@ -18,5 +20,20 @@ public class Test {
 
         //new一个对象过程
         SunNewClass sunNewClass = new SunNewClass();
+        System.out.println("===============================");
+
+        //创建对象的几种方法
+        //1 new
+        FatherNewClass f = new FatherNewClass();
+
+        //2 反射 Class的newInstance
+        Class clasz = Class.forName("com.guchaolong.javalearn.base.FatherNewClass");
+        FatherNewClass o = (FatherNewClass)clasz.newInstance();
+
+        //构造函数的newInstance
+        Constructor<FatherNewClass> constructor = FatherNewClass.class.getConstructor();
+        FatherNewClass fatherNewClass = constructor.newInstance();
+
+
     }
 }
---
+++ src/main/java/com/guchaolong/javalearn/concurrency/ThreadTest.java
@@ -0,0 +1,14 @@
+package com.guchaolong.javalearn.concurrency;
+
+/**
+ * Description:
+ *
+ * @author AA
+ * @date 2020/8/19 18:25
+ */
+public class ThreadTest extends Thread {
+    @Override
+    public void run() {
+        super.run();
+    }
+}
