2d84be6811341677b4db5e18cebc2b360b41b2b5
guchaolong <guclno1@qq.com>
2020-08-22 16:23:40 +0800

    sdfa

---
+++ src/main/java/com/guchaolong/javalearn/thread/cyclicbarrier/CyclicBarrierTest.java
@@ -0,0 +1,56 @@
+package com.guchaolong.javalearn.thread.cyclicbarrier;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Description:
+ *
+ * @author AA
+ * @date 2020/8/22 15:58
+ */
+public class CyclicBarrierTest {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        CyclicBarrier barrier = new CyclicBarrier(3);
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.submit(new Thread(new Task(barrier, "1 task")));
+        executor.submit(new Thread(new Task(barrier, "2 task")));
+        executor.submit(new Thread(new Task(barrier, "3 task")));
+
+        executor.shutdown();
+    }
+}
+
+class Task implements Runnable {
+    // 一个同步辅助类，它允许一组线程互相等待，直到到达某个公共屏障点 (common barrier point)
+    private CyclicBarrier barrier;
+
+    private String name;
+
+    public Task(CyclicBarrier barrier, String name) {
+        super();
+        this.barrier = barrier;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000 * (new Random()).nextInt(8));
+            System.out.println(name + " reading...");
+            // barrier的await方法，在所有参与者都已经在此 barrier 上调用 await 方法之前，将一直等待。
+            barrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        System.out.println(name + " go");
+    }
+}
