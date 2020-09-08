826beb23903ac922dcf25c8d217c157b496aa73c
guchaolong <guclno1@qq.com>
2020-09-09 02:23:08 +0800

    sdf

---
+++ src/main/java/com/guchaolong/javalearn/io/SimpleFileTransferTest.java
@@ -0,0 +1,39 @@
+package com.guchaolong.javalearn.io;
+
+import java.io.*;
+
+/**
+ * Description:
+ *
+ * @author AA
+ * @date 2020/9/9 2:11
+ */
+public class SimpleFileTransferTest {
+
+    public static void main(String[] args) throws Exception{
+        File source = new File("E:\\新版发布版.zip");
+        File des = new File("E:\\qqq.zip");
+        System.out.println("传统io耗时：" + transferFile(source, des));
+
+    }
+
+    private static long transferFile(File source, File des) throws Exception{
+
+        long start = System.currentTimeMillis();
+
+        if(!des.exists()){
+            des.createNewFile();
+        }
+
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(source));
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(des));
+
+        byte[] buff = new byte[1024 * 1024];
+        int len;
+        while ((len = bis.read(buff)) != -1){
+            bos.write(buff);
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+}
---
+++ src/main/java/com/guchaolong/test/Test.java
@@ -0,0 +1,14 @@
+package com.guchaolong.test;
+
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author AA
+ * @date 2020/9/7 23:08
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
