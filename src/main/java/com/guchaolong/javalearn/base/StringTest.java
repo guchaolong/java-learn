446f52e3bbadb732df808e32c6bd21c4869a0f7f
guchaolong <guclno1@qq.com>
2021-03-07 02:04:15 +0800

    dfs

--- src/main/java/com/guchaolong/javalearn/base/StringTest.java
+++ src/main/java/com/guchaolong/javalearn/base/StringTest.java
@@ -9,6 +9,8 @@ package com.guchaolong.javalearn.base;
 public class StringTest {
     public static void main(String[] args) {
         String str = "helloword";
+        String str2 = "hello".substring(1,5);
+        System.out.println("substring " + str2);
         char[] chars = new char[]{'h', 'e', 'l', 'l','o'};
         System.out.println(str + "-" +str.substring(2));//substring(n)  去掉前n个
         System.out.println(new String(chars, 1, 4));//offset: 起始位置（0开始），  count:字符个数
