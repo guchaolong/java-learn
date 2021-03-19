fa40058a65299d250e88eda75792269239458fe5
guchaolong <guclno1@qq.com>
2021-03-19 23:37:54 +0800

    >>>

--- src/main/java/com/guchaolong/javalearn/cacuchar/Test2.java
+++ src/main/java/com/guchaolong/javalearn/cacuchar/Test2.java
@@ -20,5 +20,24 @@ public class Test2 {
         System.out.println(32>>3);//4
         System.out.println(24>>>3);//3
         System.out.println(-24>>>3);//536870909
+        System.out.println(24>>>-3);//
+        System.out.println(-24>>>-3);//
+
+
+        int a = 1;
+        int b = 2;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a :" + a);
+        System.out.println("b :" + b);
+
+        int c = 1;
+        int d = 2;
+        c = c ^ d;
+        d = c ^ d;
+        c = c ^ d;
+        System.out.println("c: " + c);
+        System.out.println("d: " + d);
     }
 }
